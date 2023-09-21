c252f102f0b121ef787f18700047f07d29087a30
Jesuloba <jesulobajohn@gmail.com>
2023-09-21 23:51:27 +0100

    footer section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import PricingSection from "@/components/pricingSection";
 import TestimonialSection from "@/components/testimonialSection";
+import FooterSection from "@/components/footerSection";
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className="px-10">
-      Home page
-      {/* <PricingSection />
-      <TestimonialSection />; */}
-    </div>
-  );
+	return (
+		// <div className="px-10">
+		// 	Home page
+		// 	<PricingSection />
+		// 	<TestimonialSection />;
+		// </div>
+		<div>
+			<FooterSection />
+		</div>
+	);
 };
 
 export default HomePage;
---
+++ src/assets/socials/index.ts
@@ -0,0 +1,6 @@
+import Facebook from "./facebook.svg";
+import Instagram from "./instagram.svg";
+import Linkedin from "./linkedin.svg";
+import Twitter from "./twitter.svg";
+
+export { Facebook, Instagram, Linkedin, Twitter };
