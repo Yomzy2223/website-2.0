05f7cd8dea89a3e0e0624fce87d018f87d61bc19
Damola Olutoke <olutokeezekiel18@gmail.com>
2023-09-22 14:25:13 +0100

    section header and navbar ✅

--- src/components/landing/Hero/index.tsx
+++ src/components/landing/Hero/index.tsx
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import ArrowRight from "@/assets/icons/Arrow-right.svg";
 import ArrowRightIcon from "@/assets/icons/arrowRight";
+import { Button } from "@/components/ui/button";
+import HeroImage from "@/assets/images/hero-image.png"
+import Logo from "@/assets/icons/logo.svg";
+
 const heroText = [
   "Register",
   "Bank",
@@ -74,7 +78,7 @@ export default function HomeHeaderComponent() {
             </div>
           </div>
           <div className="text-center">
-            <h6 className="text-4xl font-bold tracking-tight text-[#4E5152] sm:text-5xl">
+            <h6 className="text-3xl font-bold tracking-tight text-[#4E5152] sm:text-4xl">
               Best place to do everything about your business.
               <span className="text-primary">Register it</span>
             </h6>
@@ -84,12 +88,15 @@ export default function HomeHeaderComponent() {
               paperwork.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
+              {/* <a
                 href="#"
                 className="bg-secondary hover:bg-primary-700 rounded-lg px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Create Account
-              </a>
+              </a> */}
+              <Button variant="pattern">
+                Create Account
+              </Button>
               <a
                 href="#"
                 className="text-secondary hover:text-secondary-600 text-sm font-semibold leading-6"
@@ -105,6 +112,93 @@ export default function HomeHeaderComponent() {
           </div>
         </div>
         <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"></div>
+
+        <div className="mt-0 bg-white py-24 sm:py-32">
+          <div className="mb-8 flex justify-center"> 
+            <Image
+              src={HeroImage}
+              width={1000}
+              height={256}
+              alt="HeroImage"
+            />
+          </div>
+
+        </div>
+       
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center">
+            <div  
+              x-data="{}"
+              x-init="$nextTick(() => {
+                  let ul = $refs.logos;
+                  ul.insertAdjacentHTML('afterend', ul.outerHTML);
+                  ul.nextSibling.setAttribute('aria-hidden', 'true');
+              })"
+              className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+              <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                  <li>
+                    <Image
+                      className="max-h-12 w-full object-contain"
+                      src="https://res.cloudinary.com/soss/image/upload/v1631100661/Sidebrief%20Assets/logo-arifu_wcjwkv.png"
+                      alt="Arifu"
+                      width={128}
+                      height={38}
+                    />
+                  </li>
+                  
+                  <li>
+                    <Image
+                        className="max-h-12 w-full object-contain"
+                        src="https://res.cloudinary.com/soss/image/upload/v1631100661/Sidebrief%20Assets/logo-54gene_bseu02.png"
+                        alt="54gene"
+                        width={128}
+                        height={38}
+                      />
+                  </li>
+                  
+                  <li>
+                    <Image
+                        className="max-h-12 w-full object-contain"
+                        src="https://res.cloudinary.com/soss/image/upload/v1631100661/Sidebrief%20Assets/logo-edubridge_lyhr9z.png"
+                        alt="Edubridge"
+                        width={128}
+                        height={38}
+                      />
+                  </li>
+                  
+                  <li>
+                    <Image
+                        className="max-h-12 w-full object-contain"
+                        src="https://res.cloudinary.com/soss/image/upload/v1631100662/Sidebrief%20Assets/logo-seamlesshr_fuh8ye.png"
+                        alt="SeamlessHR"
+                        width={128}
+                        height={38}
+                      />
+                  </li>
+                  
+                  <li>
+                    <Image
+                        className="max-h-12 w-full object-contain"
+                        src="https://res.cloudinary.com/soss/image/upload/v1631100662/Sidebrief%20Assets/logo-payqin_lg4zwc.png"
+                        alt="PayQin"
+                        width={128}
+                        height={38}
+                      />
+                  </li>
+
+                  <li>
+                    <Image
+                        className="max-h-12 w-full object-contain"
+                        src="https://res.cloudinary.com/soss/image/upload/v1631100662/Sidebrief%20Assets/logo-spleet_nvggfx.png"
+                        alt="Spleet"
+                        width={128}
+                        height={38}
+                      />
+                  </li>
+              </ul>   
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
